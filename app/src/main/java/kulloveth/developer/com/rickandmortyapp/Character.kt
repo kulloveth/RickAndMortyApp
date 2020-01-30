61ad29e743a53e66c8be7a32b34e29cb85d9df00
kulloveth <kulloveth@gmail.com>
2020-01-30 07:19:59 +0100

    data class created

--- app/src/main/java/kulloveth/developer/com/rickandmortyapp/Character.kt
+++ app/src/main/java/kulloveth/developer/com/rickandmortyapp/Character.kt
@@ -1,7 +1,8 @@
 package kulloveth.developer.com.rickandmortyapp
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Character (
     @SerializedName("name")
-    val characterName:String)
+    val characterName:String):Serializable
