03c69e4c664f5a2ce4c1f1e638b9be884296e8d0
kulloveth <kulloveth@gmail.com>
2020-01-30 07:33:17 +0100

    api interface created

--- app/src/main/java/kulloveth/developer/com/rickandmortyapp/ApiInterface.kt
+++ app/src/main/java/kulloveth/developer/com/rickandmortyapp/ApiInterface.kt
@@ -1,10 +1,11 @@
 package kulloveth.developer.com.rickandmortyapp
 
+
+import retrofit2.Call
 import retrofit2.http.GET
 
+
 interface ApiInterface {
-    //@GET("api/character/")
-    //fun fetchName(@param: )
-
-
+    @GET("api/character/")
+    fun fetchCharacterName(): Call<List<Character>>
 }
